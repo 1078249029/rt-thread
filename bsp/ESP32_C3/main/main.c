4991743c4b9bcdb3defef4930fb1e62c2608aab5
1078249029 <1078249029@qq.com>
2024-11-22 17:01:42 +0800

    prepare read mpu6050

--- bsp/ESP32_C3/main/main.c
+++ bsp/ESP32_C3/main/main.c
@@ -14,6 +14,64 @@
 #include <rtdevice.h>
 #include <board.h>
 
+#include "/home/lzx/rt-thread/rt-thread/bsp/ESP32_C3/packages/ESP-IDF-latest/tools/mocks/hal/include/hal/gpio_types.h"
+#include "/home/lzx/rt-thread/rt-thread/bsp/ESP32_C3/drivers/drv_spi.h"
+
+#define W25Q_SPI_DEVICE_NAME "spi20"
+#define SPI_BUS_NAME "spi2"
+
+void spi_w25q_sample(void)
+{
+    rt_hw_spi_device_attach(SPI_BUS_NAME, W25Q_SPI_DEVICE_NAME, GPIO_NUM_10 );
+
+    struct rt_spi_device *spi_w25q;
+    struct rt_spi_configuration cfg;
+    
+    rt_uint8_t w25x_read_id[5] = {0x9f, 0xff, 0xff, 0x00, 0xff};
+    rt_uint8_t id[5] = {0x00,0x00,0x00,0x00,0x00};
+
+    spi_w25q = (struct rt_spi_device*)rt_device_find(W25Q_SPI_DEVICE_NAME);
+    if(!spi_w25q)
+    {
+        rt_kprintf("spi asmple run failed! can't find %s device\n",W25Q_SPI_DEVICE_NAME);
+    }
+    else 
+    {    
+        cfg.max_hz = 0;
+        cfg.data_width = 7;
+        cfg.mode = RT_SPI_MODE_0;
+        rt_spi_configure(spi_w25q, &cfg);
+
+        struct rt_spi_message msg1;
+
+        msg1.send_buf   = &w25x_read_id;
+        msg1.recv_buf   = &id;
+        msg1.length     = 7;//
+        msg1.cs_take    = 1;
+        msg1.cs_release = 0;
+        msg1.next       = RT_NULL;
+
+        rt_spi_transfer_message(spi_w25q, &msg1);
+        rt_kprintf("use rt_spi_transfer_message() read w25q ID is:%x%x\n", id[2], id[3]);
+    }
+    for(int a = 0; a < sizeof(id); a++)
+    {
+        rt_kprintf("id is:%x\t", id[a]);
+    }
+}
+MSH_CMD_EXPORT(spi_w25q_sample, spi w25q sample);
+
+/*
+ *todo: todo: iic read data by mpu6050
+ *
+ */
+
+void read_mpu6050_sample(void)
+{
+
+}
+MSH_CMD_EXPORT(read_mpu6050_sample, read mpu6050 sample);
+
 #ifdef BSP_USING_BLE
 void app_main()
 {
